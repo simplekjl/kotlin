fb8a420210360ef2f92c18bfce195691fec66a36
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-01-17 14:06:23 +0300

    Raw FIR builder: support type operator calls
    Related to KT-29002

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/FirRenderer.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/FirRenderer.kt
@@ -609,6 +609,10 @@ class FirRenderer(builder: StringBuilder) : FirVisitorVoid() {
 
     override fun visitOperatorCall(operatorCall: FirOperatorCall) {
         print(operatorCall.operation.operator)
+        if (operatorCall is FirTypeOperatorCall) {
+            print("/")
+            operatorCall.type.accept(this)
+        }
         visitCall(operatorCall)
     }
 
