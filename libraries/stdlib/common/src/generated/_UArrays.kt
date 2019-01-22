74feb8d2bdbd2d2836d6d9b7174f8295cdc64028
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2019-01-22 21:19:38 +0300

    Implement sum() extension method for array of unsigned values (KT-28779)

--- libraries/stdlib/common/src/generated/_UArrays.kt
+++ libraries/stdlib/common/src/generated/_UArrays.kt
@@ -699,3 +699,59 @@ public inline fun ShortArray.toUShortArray(): UShortArray {
     return UShortArray(this.copyOf())
 }
 
+/**
+ * Returns the sum of all elements in the array.
+ */
+@kotlin.jvm.JvmName("sumOfUInt")
+@SinceKotlin("1.3")
+@ExperimentalUnsignedTypes
+public fun UIntArray.sum(): UInt {
+    var sum: UInt = 0u
+    for (element in this) {
+        sum += element
+    }
+    return sum
+}
+
+/**
+ * Returns the sum of all elements in the array.
+ */
+@kotlin.jvm.JvmName("sumOfULong")
+@SinceKotlin("1.3")
+@ExperimentalUnsignedTypes
+public fun ULongArray.sum(): ULong {
+    var sum: ULong = 0uL
+    for (element in this) {
+        sum += element
+    }
+    return sum
+}
+
+/**
+ * Returns the sum of all elements in the array.
+ */
+@kotlin.jvm.JvmName("sumOfUByte")
+@SinceKotlin("1.3")
+@ExperimentalUnsignedTypes
+public fun UByteArray.sum(): UInt {
+    var sum: UInt = 0u
+    for (element in this) {
+        sum += element
+    }
+    return sum
+}
+
+/**
+ * Returns the sum of all elements in the array.
+ */
+@kotlin.jvm.JvmName("sumOfUShort")
+@SinceKotlin("1.3")
+@ExperimentalUnsignedTypes
+public fun UShortArray.sum(): UInt {
+    var sum: UInt = 0u
+    for (element in this) {
+        sum += element
+    }
+    return sum
+}
+
